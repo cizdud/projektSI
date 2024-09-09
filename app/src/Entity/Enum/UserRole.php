2b803193def1104a6f5a659f0a7599cdcade7500
cizdud <axdudzic@gmail.com>
2024-09-09 19:29:41 +0200

    filtr po kategorii działa

--- app/src/Entity/Enum/UserRole.php
+++ app/src/Entity/Enum/UserRole.php
@@ -10,7 +10,6 @@ namespace App\Entity\Enum;
  */
 enum UserRole: string
 {
-    case ROLE_USER = 'ROLE_USER';
     case ROLE_ADMIN = 'ROLE_ADMIN';
 
     /**
@@ -21,7 +20,6 @@ enum UserRole: string
     public function label(): string
     {
         return match ($this) {
-            UserRole::ROLE_USER => 'label.role_user',
             UserRole::ROLE_ADMIN => 'label.role_admin',
         };
     }
--- app/src/Entity/User.php
+++ app/src/Entity/User.php
@@ -114,7 +114,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
-        $roles[] = UserRole::ROLE_USER->value;
+        $roles[] = UserRole::ROLE_ADMIN->value;
 
         return array_unique($roles);
     }
